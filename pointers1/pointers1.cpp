77c08dc2ca2a78c6f08901b635fd4e66d7f66df9
olegk <kbryzhenyukpc@gmail.com>
2024-11-06 23:02:17 +0200

    Добавьте файлы проекта.

---
+++ pointers1/pointers1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int* findSubarray(int* A, int sizeA, int* B, int sizeB) {
+    for (int i = 0; i <= sizeA - sizeB; ++i) {
+        bool isSubset = true;
+        for (int j = 0; j < sizeB; ++j) {
+            if (A[i + j] != B[j]) {
+                isSubset = false;
+                break;
+            }
+        }
+        if (isSubset) {
+            return &A[i];
+        }
+    }
+    return nullptr;
+}
+
+int main() {
+    int A[] = { 1, 2, 3, 4, 5, 6 };
+    int B[] = { 3, 4, 5 };
+    int* result = findSubarray(A, 6, B, 3);
+
+    if (result != nullptr) {
+        std::cout << "Found pos1 " << (result - A) << std::endl;
+    }
+    else {
+        std::cout << "NO FOUND?!?!?" << std::endl;
+    }
+
+    return 0;
+}
